cafaf52263407e67a9bcecd6ee26fba01a644fe4
Hsiaoming Yang <me@lepture.com>
2013-12-18 18:56:05 +0800

    Update build.js

--- build/build.js
+++ build/build.js
@@ -244,7 +244,9 @@ module.exports = function(img, deviation) {
       img.style.filter = 'progid:DXImageTransform.Microsoft.Blur(pixelradius=' + num*2 + ')';
     };
     func(deviation);
-    return {deviation: func, svg: false};
+    img.svg = false;
+    img.deviation = func;
+    return img;
   }
 
   var blurId = Math.random();
