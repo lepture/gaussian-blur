7b6d39983bab2fecb5b25cacd1ebae68f0100199
Hsiaoming Yang <me@lepture.com>
2013-12-18 18:17:52 +0800

    Implement the code. And it works.

---
+++ index.js
@@ -0,0 +1,82 @@
+/**
+ * Gaussian Blur
+ *
+ * A gaussian blur effect for images.
+ *
+ * Copyright (c) 2013 by Hsiaoming Yang.
+ */
+
+// Sepcial Thanks to https://github.com/finom/jQuery-Gaussian-Blur
+
+var ua = navigator.userAgent;
+var insane = ~ua.indexOf('MSIE 6') || ~ua.indexOf('MSIE 7') || ~ua.indexOf('MSIE 8');
+
+var ns = 'http://www.w3.org/2000/svg';
+
+/**
+ * Create a node of svg.
+ */
+function create(name, attrs) {
+  var element = document.createElementNS(ns, name);
+  if (attrs) {
+    for (var key in attrs) {
+      if (key === 'href') {
+        element.setAttributeNS(
+          'http://www.w3.org/1999/xlink', key, attrs[key]
+        );
+      } else {
+        element.setAttribute(key, attrs[key]);
+      }
+    }
+  }
+  return element;
+}
+
+/**
+ * Interface for developer.
+ */
+module.exports = function(img, deviation) {
+  if (insane) {
+    // wow, insane IE is awesome
+    var func = function(num) {
+      img.style.filter = 'progid:DXImageTransform.Microsoft.Blur(pixelradius=' + num*2 + ')';
+    };
+    func(deviation);
+    return {deviation: func, svg: false};
+  }
+
+  var blurId = Math.random();
+
+  var svg = create('svg', {
+    version: '1.1',
+    width: img.width,
+    height: img.height,
+    id: 'svg-blur-' + blurId
+  });
+  var filter = create('filter', {id: 'svg-filter-' + blurId});
+  var gaussian = create('feGaussianBlur', {
+    'in': 'SourceGraphic',
+    stdDeviation: deviation || 2
+  });
+  var image = create('image', {
+    x: 0,
+    y: 0,
+    width: img.width,
+    height: img.height,
+    href: img.src,
+    style: 'filter:url(#svg-filter-' + blurId + ')'
+  });
+
+  filter.appendChild(gaussian);
+  svg.appendChild(filter);
+  svg.appendChild(image);
+
+  // a function for developer changing deviation
+  svg.deviation = function(num) {
+    gaussian.setAttribute('stdDeviation', num);
+  };
+  svg.svg = true;
+
+  img.parentNode.replaceChild(svg, img);
+  return svg;
+};
